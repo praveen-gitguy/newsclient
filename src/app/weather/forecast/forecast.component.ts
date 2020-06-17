272da34bee7b5e6e127f30dd2c398be526cc0e9f
Praveen Kumar <praveenkumarofficials@gmail.com>
2020-06-17 13:42:47 +0530

    Added weather widget from openweathermap api

---
+++ src/app/weather/forecast/forecast.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ForecastService } from '../forecast.service';
+
+@Component({
+  selector: 'app-forecast',
+  templateUrl: './forecast.component.html',
+  styleUrls: ['./forecast.component.css']
+})
+export class ForecastComponent implements OnInit {
+
+  forecastData = []
+  constructor(private forecastService: ForecastService) {
+    this.forecastService.getForecast()
+      .subscribe((forecastData) => {
+        this.forecastData = forecastData;
+      });
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
