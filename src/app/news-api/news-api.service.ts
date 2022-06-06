acb4b213e913e2055006fe8d0ac1d4ef4240f825
Praveen Kumar <praveenkumarofficials@gmail.com>
2022-06-06 20:23:18 +0530

    removed keys

--- src/app/news-api/news-api.service.ts
+++ src/app/news-api/news-api.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@angular/core';
-import { Subject, Observable, throwError } from 'rxjs';
-import { map, switchMap, tap, pluck, catchError } from 'rxjs/operators';
-import { HttpParams, HttpClient } from '@angular/common/http';
-import { NotificationsService } from '../notifications/notifications.service';
+import { Injectable } from "@angular/core";
+import { Subject, Observable, throwError } from "rxjs";
+import { map, switchMap, tap, pluck, catchError } from "rxjs/operators";
+import { HttpParams, HttpClient } from "@angular/common/http";
+import { NotificationsService } from "../notifications/notifications.service";
 
 export interface Article {
-  title: string,
-  url: string,
-  urlToImage:string,
+  title: string;
+  url: string;
+  urlToImage: string;
   source: {
     name: string;
-  }
+  };
 }
 
 export interface NewsApiResponse {
@@ -19,13 +19,13 @@ export interface NewsApiResponse {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class NewsApiService {
-  private url = 'https://newsapi.org/v2/top-headlines';
+  private url = "https://newsapi.org/v2/top-headlines";
   private pageSize = 10;
-  private apiKey = 'b7930452247b440c8d5dcd991ffe0632';
-  private country = 'in';
+  private apiKey = "";
+  private country = "in";
 
   private pagesInput: Subject<number>;
   pagesOutput: Observable<Article[]>;
@@ -34,28 +34,28 @@ export class NewsApiService {
   constructor(
     private http: HttpClient,
     private notifications: NotificationsService
-    ) {
+  ) {
     this.numberOfPages = new Subject();
     this.pagesInput = new Subject();
     this.pagesOutput = this.pagesInput.pipe(
       map((page) => {
         return new HttpParams()
-          .set('apiKey', this.apiKey)
-          .set('country', this.country)
-          .set('pageSize', String(this.pageSize))
-          .set('page', String(page));
+          .set("apiKey", this.apiKey)
+          .set("country", this.country)
+          .set("pageSize", String(this.pageSize))
+          .set("page", String(page));
       }),
       switchMap((params) => {
         return this.http.get<NewsApiResponse>(this.url, { params });
       }),
-      tap(response => {
+      tap((response) => {
         const totalPages = Math.ceil(response.totalResults / this.pageSize);
         this.numberOfPages.next(totalPages);
         // this.notifications.addSuccess('News fetched');
       }),
-      pluck('articles'),
-      catchError(err => {
-        this.notifications.addError('Can\'t fetch news at the moment');
+      pluck("articles"),
+      catchError((err) => {
+        this.notifications.addError("Can't fetch news at the moment");
         return throwError(err);
       })
     );
@@ -64,5 +64,4 @@ export class NewsApiService {
   getPage(page: number) {
     this.pagesInput.next(page);
   }
-
 }
--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,47 +1,57 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs';
-import { map, switchMap, pluck, mergeMap, filter, toArray, share, tap, catchError, retry } from 'rxjs/operators';
-import { NotificationsService } from '../notifications/notifications.service';
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable, of, throwError } from "rxjs";
+import {
+  map,
+  switchMap,
+  pluck,
+  mergeMap,
+  filter,
+  toArray,
+  share,
+  tap,
+  catchError,
+  retry,
+} from "rxjs/operators";
+import { NotificationsService } from "../notifications/notifications.service";
 
 interface OpenWeatherResponse {
   list: {
     dt_txt: string;
     main: {
       temp: number;
-    }
+    };
   }[];
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ForecastService {
-  private url = 'https://api.openweathermap.org/data/2.5/forecast';
+  private url = "https://api.openweathermap.org/data/2.5/forecast";
 
   constructor(
     private http: HttpClient,
     private notificationsService: NotificationsService
-  ) { }
-
+  ) {}
 
   getCurrentLocation() {
-    return new Observable<Coordinates>(observer => {
+    return new Observable<Coordinates>((observer) => {
       window.navigator.geolocation.getCurrentPosition(
-        position => {
+        (position) => {
           observer.next(position.coords);
           observer.complete();
         },
-        err => observer.error(err)
+        (err) => observer.error(err)
       );
     }).pipe(
       tap(() => {
-        this.notificationsService.addSuccess('Location fetched successfully.')
+        this.notificationsService.addSuccess("Location fetched successfully.");
       }),
       catchError((err) => {
         // #1 - Handle the error
-        this.notificationsService.addError('Failed to get location');
+        this.notificationsService.addError("Failed to get location");
         // #2 - Return a new observable
         return throwError(err);
       })
@@ -49,29 +59,30 @@ export class ForecastService {
   }
 
   getForecast() {
-    return this.getCurrentLocation()
-      .pipe(
-        map(coords => {
-          return new HttpParams()
-            .set('lat', String(coords.latitude))
-            .set('lon', String(coords.longitude))
-            .set('units', 'metric')
-            .set('appid', '8db32ba2fee31e1106dd1852602d4db2')
-        }),
-        switchMap(params => this.http.get<OpenWeatherResponse>(this.url, { params })),
-        pluck('list'),
-        mergeMap(value => of(...value)),
-        filter((value, index) => {
-          return index % 8 === 0;
-        }),
-        map((value) => {
-          return {
-            dateString: value.dt_txt,
-            temp: value.main.temp
-          }
-        }),
-        toArray(),
-        share()
-      )
+    return this.getCurrentLocation().pipe(
+      map((coords) => {
+        return new HttpParams()
+          .set("lat", String(coords.latitude))
+          .set("lon", String(coords.longitude))
+          .set("units", "metric")
+          .set("appid", "");
+      }),
+      switchMap((params) =>
+        this.http.get<OpenWeatherResponse>(this.url, { params })
+      ),
+      pluck("list"),
+      mergeMap((value) => of(...value)),
+      filter((value, index) => {
+        return index % 8 === 0;
+      }),
+      map((value) => {
+        return {
+          dateString: value.dt_txt,
+          temp: value.main.temp,
+        };
+      }),
+      toArray(),
+      share()
+    );
   }
 }
